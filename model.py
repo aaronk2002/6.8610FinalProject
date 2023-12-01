2e223e106579242f609d4a179c9ee09118070fe2
Aaron Alvarado K. Julistiono <aaron25@mit.edu>
2023-12-01 15:40:40 -0500

    Changes made to make it exectuable in Supercloud

--- model.py
+++ model.py
@@ -10,13 +10,24 @@ import random
 import utils
 
 import torch
-from tensorboardX import SummaryWriter
+
+# from tensorboardX import SummaryWriter
 from progress.bar import Bar
 
 
 class MusicTransformer(torch.nn.Module):
-    def __init__(self, embedding_dim=256, vocab_size=388+2, num_layer=6,
-                 max_seq=2048, dropout=0.2, debug=False, loader_path=None, dist=False, writer=None):
+    def __init__(
+        self,
+        embedding_dim=256,
+        vocab_size=388 + 2,
+        num_layer=6,
+        max_seq=2048,
+        dropout=0.2,
+        debug=False,
+        loader_path=None,
+        dist=False,
+        writer=None,
+    ):
         super().__init__()
         self.infer = False
         if loader_path is not None:
@@ -31,32 +42,43 @@ class MusicTransformer(torch.nn.Module):
 
         self.writer = writer
         self.Decoder = Encoder(
-            num_layers=self.num_layer, d_model=self.embedding_dim,
-            input_vocab_size=self.vocab_size, rate=dropout, max_len=max_seq)
+            num_layers=self.num_layer,
+            d_model=self.embedding_dim,
+            input_vocab_size=self.vocab_size,
+            rate=dropout,
+            max_len=max_seq,
+        )
         self.fc = torch.nn.Linear(self.embedding_dim, self.vocab_size)
 
     def forward(self, x, length=None, writer=None):
         if self.training or not self.infer:
-            _, _, look_ahead_mask = utils.get_masked_with_pad_tensor(self.max_seq, x, x, config.pad_token)
+            _, _, look_ahead_mask = utils.get_masked_with_pad_tensor(
+                self.max_seq, x, x, config.pad_token
+            )
             decoder, w = self.Decoder(x, mask=look_ahead_mask)
             fc = self.fc(decoder)
-            return fc.contiguous() if self.training else (fc.contiguous(), [weight.contiguous() for weight in w])
+            return (
+                fc.contiguous()
+                if self.training
+                else (fc.contiguous(), [weight.contiguous() for weight in w])
+            )
         else:
             return self.generate(x, length, None).contiguous().tolist()
 
-    def generate(self,
-                 prior: torch.Tensor,
-                 length=2048,
-                 tf_board_writer: SummaryWriter = None):
+    def generate(self, prior: torch.Tensor, length=2048, tf_board_writer=None):
         decode_array = prior
         result_array = prior
         print(config)
         print(length)
-        for i in Bar('generating').iter(range(length)):
+        for i in Bar("generating").iter(range(length)):
             if decode_array.size(1) >= config.threshold_len:
                 decode_array = decode_array[:, 1:]
-            _, _, look_ahead_mask = \
-                utils.get_masked_with_pad_tensor(decode_array.size(1), decode_array, decode_array, pad_token=config.pad_token)
+            _, _, look_ahead_mask = utils.get_masked_with_pad_tensor(
+                decode_array.size(1),
+                decode_array,
+                decode_array,
+                pad_token=config.pad_token,
+            )
 
             # result, _ = self.forward(decode_array, lookup_mask=look_ahead_mask)
             # result, _ = decode_fn(decode_array, look_ahead_mask)
