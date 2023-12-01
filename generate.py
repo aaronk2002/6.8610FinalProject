d1b81c93547c3bd3ce86a2307f98304805a8e55e
Aaron Alvarado K. Julistiono <aaron25@mit.edu>
2023-12-01 18:31:55 -0500

    fixed generate.py

--- generate.py
+++ generate.py
@@ -7,10 +7,12 @@ from data import Data
 import utils
 from midi_processor.processor import decode_midi, encode_midi
 
+import torch
+
 import datetime
 import argparse
 
-from tensorboardX import SummaryWriter
+# from tensorboardX import SummaryWriter
 
 
 parser = custom.get_argument_parser()
@@ -19,36 +21,28 @@ config.load(args.model_dir, args.configs, initialize=True)
 
 # check cuda
 if torch.cuda.is_available():
-    config.device = torch.device('cuda')
+    config.device = torch.device("cuda")
 else:
-    config.device = torch.device('cpu')
+    config.device = torch.device("cpu")
 
 
-current_time = datetime.datetime.now().strftime('%Y%m%d-%H%M%S')
-gen_log_dir = 'logs/mt_decoder/generate_'+current_time+'/generate'
-gen_summary_writer = SummaryWriter(gen_log_dir)
+# current_time = datetime.datetime.now().strftime('%Y%m%d-%H%M%S')
+# gen_log_dir = 'logs/mt_decoder/generate_'+current_time+'/generate'
+# gen_summary_writer = SummaryWriter(gen_log_dir)
 
-mt = MusicTransformer(
-    embedding_dim=config.embedding_dim,
-    vocab_size=config.vocab_size,
-    num_layer=config.num_layers,
-    max_seq=config.max_seq,
-    dropout=0,
-    debug=False)
-mt.load_state_dict(torch.load(args.model_dir+'/final.pth'))
+mt = torch.load("config/final.pth").to(config.device)
 mt.test()
 
 print(config.condition_file)
 if config.condition_file is not None:
-    inputs = np.array([encode_midi('dataset/midi/BENABD10.mid')[:500]])
+    inputs = np.array([encode_midi("dataset/midi/BENABD10.mid")[:500]])
 else:
     inputs = np.array([[24, 28, 31]])
-inputs = torch.from_numpy(inputs)
-result = mt(inputs, config.length, gen_summary_writer)
+inputs = torch.from_numpy(inputs).to(config.device)
+result = mt(inputs, config.length)
 
-for i in result:
-    print(i)
+# torch.save(result, "generated.pt")
 
-decode_midi(result, file_path=config.save_path)
+decode_midi(result, file_path="generated.mid")
 
-gen_summary_writer.close()
+# gen_summary_writer.close()
