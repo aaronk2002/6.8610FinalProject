519d99c6c448de447b56d04badc89b1d0cb95e7b
Aaron Alvarado K. Julistiono <aaron25@mit.edu>
2023-12-03 21:04:11 -0500

    Preprocessed Maestro midi files

--- preprocess.py
+++ preprocess.py
@@ -11,27 +11,27 @@ def preprocess_midi(path):
 
 
 def preprocess_midi_files_under(midi_root, save_dir):
-    midi_paths = list(utils.find_files_by_extensions(midi_root, ['.mid', '.midi']))
+    midi_paths = list(utils.find_files_by_extensions(midi_root, [".mid", ".midi"]))
     os.makedirs(save_dir, exist_ok=True)
-    out_fmt = '{}-{}.data'
+    out_fmt = "{}-{}.data"
 
-    for path in Bar('Processing').iter(midi_paths):
-        print(' ', end='[{}]'.format(path), flush=True)
+    for path in Bar("Processing").iter(midi_paths):
+        print(" ", end="[{}]".format(path), flush=True)
 
         try:
             data = preprocess_midi(path)
         except KeyboardInterrupt:
-            print(' Abort')
+            print(" Abort")
             return
         except EOFError:
-            print('EOF Error')
+            print("EOF Error")
             return
 
-        with open('{}/{}.pickle'.format(save_dir, path.split('/')[-1]), 'wb') as f:
+        with open(
+            "{}/{}.pickle".format(save_dir, path.split("/")[-1].split("\\")[-1]), "wb"
+        ) as f:
             pickle.dump(data, f)
 
 
-if __name__ == '__main__':
-    preprocess_midi_files_under(
-            midi_root=sys.argv[1],
-            save_dir=sys.argv[2])
+if __name__ == "__main__":
+    preprocess_midi_files_under(midi_root=sys.argv[1], save_dir=sys.argv[2])
