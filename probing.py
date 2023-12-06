1e26c717e625a7c6f067aea2249d23e67e93caba
Aaron Alvarado K. Julistiono <aaron25@mit.edu>
2023-12-06 16:34:11 -0500

    Updated probing.py to make probing tasks

--- probing.py
+++ probing.py
@@ -3,6 +3,9 @@ import pickle
 import utils
 import math
 import random
+import pandas as pd
+from data import Data
+import argparse
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 pad_token = 388
@@ -134,17 +137,44 @@ def probe_regressor(X, y, lr, epochs):
 
 
 if __name__ == "__main__":
-    X, y = probe_task(
-        [
-            "dataset/processed/MIDI-Unprocessed_01_R1_2006_01-09_ORIG_MID--AUDIO_01_R1_2006_01_Track01_wav.midi.pickle"
-        ],
-        "trained_models/8.pth",
-        {
-            "MIDI-Unprocessed_01_R1_2006_01-09_ORIG_MID--AUDIO_01_R1_2006_01_Track01_wav.midi.pickle": [
-                1
-            ]
-        },
-        10,
+    # Arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--model", help="model path", type=str)
+    parser.add_argument("--save", help="saving path", type=str)
+    args = parser.parse_args()
+    print(f"model = {args.model}, save = {args.save}")
+
+    # Dataframe
+    df = pd.read_csv("dataset/maestro_new.csv")
+
+    # Label to idx
+    key_to_idx = {"major": 1, "minor": 0}
+    composers = df["canonical_composer"].unique()
+    composers = [composer for composer in composers if "/" not in composer]
+    composers.sort()
+    composer_to_idx = {composer: idx for idx, composer in enumerate(composers)}
+
+    # File to label
+    label = {}
+    for idx, row in df.iterrows():
+        if "/" in row["canonical_composer"]:
+            continue
+        label[row["midi_filename"][5:] + ".pickle"] = [
+            composer_to_idx[row["canonical_composer"]],
+            key_to_idx[row["key_mode"]],
+        ]
+
+    # Processing paths
+    dataset = Data("dataset/processed")
+    train_path = [file.split("\\")[-1] for file in dataset.file_dict["train"]]
+    train_path = ["dataset/processed/" + file for file in train_path if file in label]
+    eval_path = [file.split("\\")[-1] for file in dataset.file_dict["eval"]]
+    eval_path = ["dataset/processed/" + file for file in eval_path if file in label]
+
+    # Get probe datasets
+    train_x, train_y = probe_task(train_path, args.model, label, 1)
+    eval_x, eval_y = probe_task(eval_path, args.model, label, 1)
+    torch.save(
+        {"train_x": train_x, "train_y": train_y, "eval_x": eval_x, "eval_y": eval_y},
+        args.save,
     )
-    print(X.shape)
-    # models = probe_classifier(X, y.to(torch.long), 3, 0.1, 10)
