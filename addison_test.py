93efa3a934d74dd2e40aea56c0120e4a1309c4e0
Aaron Alvarado K. Julistiono <aaron25@mit.edu>
2023-12-06 09:03:11 -0500

    Add trained models from supercloud and finish probing module

--- addison_test.py
+++ addison_test.py
@@ -1,11 +1,16 @@
 import pickle
 from midi_processor.processor import decode_midi, encode_midi
 import torch
+import random
 
-with open(
-    "test_decode/2018/MIDI-Unprocessed_Chamber2_MID--AUDIO_09_R3_2018_wav--1.midi.pickle",
-    "rb",
-) as f:
-    data = pickle.load(f)
+# with open(
+#     "test_decode/2018/MIDI-Unprocessed_Chamber2_MID--AUDIO_09_R3_2018_wav--1.midi.pickle",
+#     "rb",
+# ) as f:
+#     data = pickle.load(f)
+
+data = []
+for _ in range(100_000):
+    data.append(random.randint(0, 387))
 
 decode_midi(data, file_path="test.mid")
--- probing.py
+++ probing.py
@@ -2,12 +2,14 @@ import torch
 import pickle
 import utils
 import math
+import random
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 pad_token = 388
+random.seed(1)
 
 
-def probe_task(input_paths, model_path, label):
+def probe_task(input_paths, model_path, label, M):
     """
     Given the inputs, the trained transformer model, and the label, generate
     several datasets for the probing tasks, one dataset for each layer
@@ -18,13 +20,15 @@ def probe_task(input_paths, model_path, label):
                       each as a forward slash separated string
     - model_path    : file adress for the transformer model
     - label         : dictionary mapping midi filename to the desired
-                      label, and it must contain all midi filenames in
-                      input_paths
+                      label (list with d' elements), and it must contain
+                      all midi filenames in input_paths
+    - M             : number of samples per file
 
     Return:
-    torch tensor with size num_layer x N x d for input and torch tensor
-    with size N for label, N is number of samples, d is length of samples,
-    and num_layer is the number of layers
+    torch tensor with size num_layer x N*M x d for input and torch tensor
+    with size N*M x (d'+1) for label, where d is length of samples, num_layer
+    is the number of layers, N is the number of files in the input_paths,
+    and d' is the dimensionality of each label, extra one for control
     """
     # Model information
     mt = torch.load(model_path, map_location=torch.device("cpu")).to(device)
@@ -39,8 +43,11 @@ def probe_task(input_paths, model_path, label):
     for path in input_paths:
         with open(path, "rb") as f:
             data = pickle.load(f)
-            raw_data.append(data[:max_seq])
-            y.append(label[path.split("/")[-1]])
+            for _ in range(M):
+                idx = random.randint(0, len(data) - max_seq)
+                control = sum(data[idx : idx + max_seq])
+                raw_data.append(data[idx : idx + max_seq])
+                y.append(label[path.split("/")[-1]] + [control])
     x = torch.Tensor(raw_data)
     y = torch.Tensor(y)
 
@@ -51,12 +58,79 @@ def probe_task(input_paths, model_path, label):
     x = mt.Decoder.pos_encoding(x)
 
     # Output per layer
-    X = torch.zeros((num_layer, N, emb_dim * max_seq))
+    X = torch.zeros((num_layer, N * M, emb_dim))
     for idx in range(num_layer):
         x, _ = mt.Decoder.enc_layers[idx](x, look_ahead_mask)
-        X[idx] = x.reshape(N, -1)
+        X[idx] = x.mean(dim=1).detach()
 
     return X, y
+
+
+def probe_classifier(X, y, num_classes, lr, epochs):
+    """
+    Given a set of input dataset and output, create a linear classifier
+    model for each set of inputs
+
+    Input:
+    - X             : inputs with size num_model x N x d
+    - y             : labels with size N
+    - num_classes   : number of classes
+    - lr            : learning step for optimization
+    - epochs        : number of epochs
+
+    Output:
+    list of num_model models
+    """
+    # Initialization
+    num_model, N, d = X.shape
+    models = [
+        torch.nn.Sequential(torch.nn.Linear(d, num_classes), torch.nn.LogSoftmax(dim=1))
+        for _ in range(num_model)
+    ]
+    loss_fn = torch.nn.NLLLoss()
+
+    # Train N models
+    for idx in range(N):
+        model = models[idx]
+        optimizer = torch.optim.SGD(model.parameters(), lr=lr)
+        model.train()
+        for _ in range(epochs):
+            logits = model(X[idx])
+            loss = loss_fn(logits, y)
+            loss.backward()
+            optimizer.step()
+    return models
+
+
+def probe_regressor(X, y, lr, epochs):
+    """
+    Given a set of input dataset and output, create a linear regressor
+    model for each set of inputs
+
+    Input:
+    - X             : inputs with size num_model x N x d
+    - y             : labels with size N
+
+    Output:
+    list of num_model models
+    """
+    # Initialization
+    num_model, N, d = X.shape
+    models = [torch.nn.Linear(d, 1) for _ in range(num_model)]
+    loss_fn = torch.nn.MSELoss()
+
+    # Train N models
+    for idx in range(N):
+        model = models[idx]
+        optimizer = torch.optim.SGD(model.parameters(), lr=lr)
+        model.train()
+        for _ in range(epochs):
+            pred = model(X[idx])
+            optimizer.zero_grad()
+            loss = loss_fn(pred.reshape(-1), y)
+            loss.backward()
+            optimizer.step()
+    return models
 
 
 if __name__ == "__main__":
@@ -66,7 +140,11 @@ if __name__ == "__main__":
         ],
         "trained_models/8.pth",
         {
-            "MIDI-Unprocessed_01_R1_2006_01-09_ORIG_MID--AUDIO_01_R1_2006_01_Track01_wav.midi.pickle": 1
+            "MIDI-Unprocessed_01_R1_2006_01-09_ORIG_MID--AUDIO_01_R1_2006_01_Track01_wav.midi.pickle": [
+                1
+            ]
         },
+        10,
     )
     print(X.shape)
+    # models = probe_classifier(X, y.to(torch.long), 3, 0.1, 10)
