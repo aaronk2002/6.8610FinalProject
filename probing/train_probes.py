152dcddf7ac04ea44cd4eb10486fa811b81cdb6a
Andrew Zhao <aazhao@mit.edu>
2023-12-10 01:01:39 -0500

    converted to CLI arg

--- probing/train_probes.py
+++ probing/train_probes.py
@@ -3,8 +3,23 @@ import pandas as pd
 import argparse
 from probing import probe_classifier, probe_regressor
 
+### GLOABL VARIABLES ###
+CONTROL_PATH = 'control.pth'
+COMPOSER_PATH = 'composer.pth'
+KEY_PATH = 'key.pth'
+LR = 0.1
+EPOCHS = 100000
 # Dataframe
 df = pd.read_csv("../dataset/maestro_new.csv")
+
+### GET ARGUMENTS ###
+parser = argparse.ArgumentParser()
+parser.add_argument("--num_layers", help="number of decoder layers", type=str)
+parser.add_argument("--control", help="Control type (key, composer, control)", type=str)
+args = parser.parse_args()
+control, num_layers = args.control, args.num_layers
+
+print(f'num_layers = {num_layers} control = {control} ')
 
 # Label to idx
 key_to_idx = {"major": 1, "minor": 0}
@@ -13,44 +28,33 @@ composers = [composer for composer in composers if "/" not in composer]
 composers.sort()
 composer_to_idx = {composer: idx for idx, composer in enumerate(composers)}
 
-# Get dataset
-results = {
-    idx: torch.load(
-        f"../dataset/{idx}-layers-probe.pth", map_location=torch.device("cpu")
-    )
-    for idx in [4, 6, 8]
-}
+# Get dataset with `num_layers` layers
+result = torch.load('f../dataset/{num_layers}-layers-probe.pth', map_location=torch.device("cpu"))
 
 # Train models
-key_models = {}
-composer_models = {}
-control_models = {}
-for idx in [4, 6, 8]:
-    print("Key Model")
-    key_models[idx] = probe_classifier(
-        results[idx]["train_x"],
-        results[idx]["train_y"][:, 1].to(torch.long),
+if control == 'key':
+    key_model = probe_classifier(
+        result['train_x'],
+        result['train_y'][:, 1].to(torch.long),
         len(key_to_idx),
-        0.1,
-        100000,
+        LR,
+        EPOCHS,
     )
-    print("Composer Model")
-    composer_models[idx] = probe_classifier(
-        results[idx]["train_x"],
-        results[idx]["train_y"][:, 0].to(torch.long),
+    torch.save(key_model, KEY_PATH)
+elif control == 'composer':
+    composer_model = probe_classifier(
+        result['train_x'],
+        result['train_y'][:, 0].to(torch.long),
         len(composer_to_idx),
-        0.1,
-        100000,
+        LR,
+        EPOCHS,
     )
-    print("Control Model")
-    control_models[idx] = probe_regressor(
-        results[idx]["train_x"],
-        results[idx]["train_y"][:, 2],
-        0.1,
-        100000,
+    torch.save(composer_model, COMPOSER_PATH)
+elif control == 'control':
+    control_model = probe_regressor(
+        result['train_x'],
+        result['train_y'][:, 2],
+        LR,
+        EPOCHS,
     )
-
-# Save models
-torch.save(control_models, "control.pth")
-torch.save(composer_models, "composer.pth")
-torch.save(key_models, "key.pth")
+    torch.save(control_model, CONTROL_PATH)
